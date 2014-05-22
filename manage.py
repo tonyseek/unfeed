5e4ec3f6acb3d68df921a382eede4722cac25a2d
Jiangge Zhang <tonyseek@gmail.com>
2014-05-23 03:15:17 +0800

    add fetching script.

--- manage.py
+++ manage.py
@@ -26,5 +26,19 @@ def syncdb(destory=False, verbose=False):
     db_created.send(db)
 
 
+@manager.command
+def fetch():
+    """Fetches to offline."""
+    from unfeed.models import Site, sync_indexes, sync_articles
+    for site in Site.query:
+        indexes = sync_indexes(site)
+        articles = sync_articles(indexes)
+        for article in articles:
+            print('%s: %s: %s: %s' % (
+                article.title, article.item_id, article.author,
+                article.published))
+        print('== %d articles in %s' % (len(articles), site.name))
+
+
 if __name__ == '__main__':
     manager.run()
