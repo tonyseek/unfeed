60450d57d9cb7dfe12174b554d8e55e336c809b0
Jiangge Zhang <tonyseek@gmail.com>
2014-05-23 03:15:51 +0800

    expose offline article.

--- unfeed/models/__init__.py
+++ unfeed/models/__init__.py
@@ -1,5 +1,6 @@
 from .site import Site, Category
-from .offline import OfflineIndex, sync_indexes, sync_articles
+from .offline import OfflineIndex, OfflineArticle, sync_indexes, sync_articles
 
 
-__all__ = ['Site', 'Category', 'OfflineIndex', 'sync_indexes', 'sync_articles']
+__all__ = ['Site', 'Category', 'OfflineIndex', 'OfflineArticle',
+           'sync_indexes', 'sync_articles']
